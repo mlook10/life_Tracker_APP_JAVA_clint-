2aeb14db787de2e10ad3546a0015b21a3829642a
mlook10 <144014617+mlook10@users.noreply.github.com>
2023-12-20 17:44:41 +0300

    Add files via upload

---
+++ src/main/java/Programs/WorkoutObserver.java
@@ -0,0 +1,20 @@
+package Programs;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class WorkoutObserver implements Observer {
+    // Constructor
+    public WorkoutObserver() {
+        // Initialize any necessary resources for the observer
+    }
+
+    // Implement the update method from the Observer interface
+    @Override
+    public void update(String message) {
+        // Implement the logic to receive and display workout program notifications
+    }
+}
